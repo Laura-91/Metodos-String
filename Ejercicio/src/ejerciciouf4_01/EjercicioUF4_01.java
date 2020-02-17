8501009640cff423a727514fdc723401061a9258
Laura <lauraagudo1991@gmail.com>
2020-02-17 20:09:39 +0100

    add ejemplo metodo substring

--- Ejercicio/src/ejerciciouf4_01/EjercicioUF4_01.java
+++ Ejercicio/src/ejerciciouf4_01/EjercicioUF4_01.java
@@ -1,57 +1,35 @@
-package ejerciciouf4_01;
+﻿package ejerciciouf4_01;
 
 import java.util.Scanner;
 
 public class EjercicioUF4_01 {
 
     public static void main(String[] args) {
-        String matrizIngles[][] = {         // matriz de dos dimensiones
-            {"bear","oso"},
-            {"cat","gato"},
-            {"cheetah","guepardo"},
-            {"dog","perro"},
-            {"eagle","águila"},
-            {"fish","pez"},
-            {"frog","rana"},
-            {"horse","caballo"},
-            {"lion","león"},
-            {"lizard","lagarto"},
-            {"meerkat","suricata"},
-            {"monkey","mono"},
-            {"orangutan","orangután"},
-            {"panther","pantera"},
-            {"parrot","loro"},
-            {"pig","cerdo"},
-            {"tiger","tigre"},
-            {"toad","sapo"},
-            {"tortoise","tortuga"},
-            {"mare","yegua"}
-        };
-        int cont_aciertos=0;    //inicializa contador de aciertos a 0
-        int cont_fallos=0;      //inicializa contador de fallos a 0
-        Scanner lector = new Scanner(System.in);    //crea el objeto Scanner para introducir datos por teclado
-        for(int i=0;i<matrizIngles.length;i++){     //este for recorre las filas de la matriz
-            String nombre;
-            System.out.println("Traduce "+matrizIngles[i][0]);
-            nombre = lector.nextLine();             // guarda en la variable nombre los datos introducidos por teclado
-            for(int j=1; j<matrizIngles[0].length;j++){     //recorre las columnas de la matriz 
-                
-                if (nombre.equals(matrizIngles[i][j])){     //el método .equals compara el valor introducido por teclado 
-                                                            //y el valor de la posición en cuestión de la matriz
-                    System.out.println("Bien!");
-                    cont_aciertos++;
-                }
-                else{
-                   System.out.println("No, es "+ matrizIngles[i][j]);
-                   cont_fallos++; 
-                }
-            }
-        }
-        lector.close();
-        System.out.println("FIN DEL TEST");
-        System.out.println("Has tenido "+cont_aciertos+" aciertos.");
-        System.out.println("Has tenido "+cont_fallos+" fallos.");
+        Scanner lector = new Scanner(System.in);
+        String nombre;
+        System.out.println("Escribe un texto:");
+        nombre = lector.nextLine();
+        System.out.println("Longitud: "+nombre.length());   //metodo .length() para saber la longitud del texto introducido
+        System.out.println("Mayúsucula: "+nombre.toUpperCase());    //metodo .toUpperCase() para poner el texto en mayúsulas
+        System.out.println("Palabra a palabra");
+        String[] array = nombre.split("-");   //El metodo .split() separa muestra el texto separado por palabras
+        for (int i=0;i<array.length;i++){     
+            System.out.println(array[i]);   
+        } 
+        int min=0;  //inicializo variable para contar las minúsculas
+        for (int k=0;k<nombre.length();k++){   // con este for recorre el texto y cuenta las letras minúsculas
+            if (Character.isLowerCase(nombre.charAt(k)))
+                min++; 
+        } 
+        System.out.println("Hay "+min+" letras minúsculas");  
         
+       for (int i=0;i<nombre.length();i++){ // recorre el texto y se muestra un triángulo por el metodo .substring()
+                System.out.println(nombre.substring(0, nombre.length()-i));
+        }
+
+        lector.close();
+
     }
     
 }
+
